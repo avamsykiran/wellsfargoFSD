79c9af6842ac325e489e7845d92b5f5295656476
Vamsy Kiran Aripaka <a.vamc.it@gmail.com>
2020-09-04 11:05:24 +0530

    JPQL updated

--- hibernate/jpa-hibernate-demo-3/src/main/java/com/wellsfargo/jpahd/ui/RetrivalUI.java
+++ hibernate/jpa-hibernate-demo-3/src/main/java/com/wellsfargo/jpahd/ui/RetrivalUI.java
@@ -19,13 +19,28 @@ public class RetrivalUI {
 		}
 		System.out.println("---------------------------------------------");
 
-		//retrive data on OneToOne
+		//retrive data on OneToOne (CROSS JOIN)
 		List<Object[]> data2 = em.createQuery("SELECT e.firstName,e.salAccount.bank,e.salAccount.branch FROM Employee e").getResultList();
 		for(Object[] row : data2) {
 			System.out.println(row[0] + "\t" +row[1] +"\t"+row[2]);
 		}
 		System.out.println("---------------------------------------------");
 
+		//retrive data on OneToOne (INNER JOIN)
+		List<Object[]> data3 = em.createQuery("SELECT e.firstName,b.bank,b.branch FROM Employee e JOIN e.salAccount b").getResultList();
+		for(Object[] row : data3) {
+			System.out.println(row[0] + "\t" +row[1] +"\t"+row[2]);
+		}
+		System.out.println("---------------------------------------------");
+				
+		//retrive Aggregate data 
+		List<Object[]> data4 = em.createQuery("SELECT d.deptName,COUNT(e),SUM(e.basic) FROM Department d JOIN d.employees e GROUP BY d.deptName").getResultList();
+		for(Object[] row : data4) {
+			System.out.println(row[0] + "\t" +row[1] +"\t"+row[2]);
+		}
+		System.out.println("---------------------------------------------");
+		
+		
 		
 		em.close();
 	}
